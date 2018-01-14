f83aedf7f7112bc3679bf2b623375765ecba48cf
LyricalMaestro <shinichitanimoto0120@gmail.com>
2018-01-15 01:00:02 +0900

    TensorFlowモデルを使ってMNISTの認識ができるように修正。

--- app/src/main/java/com/lyricaloriginal/mnistandroid/MainActivity.kt
+++ app/src/main/java/com/lyricaloriginal/mnistandroid/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.lyricaloriginal.mnistandroid
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import com.lyricaloriginal.mnistandroid.recoginitions.ImageClassifier
 
 class MainActivity : AppCompatActivity() {
+
+    var imageClassifier : ImageClassifier? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.classify_btn1).setOnClickListener{
+            val bitmap = BitmapFactory.decodeStream(assets.open("sampleimages/test0.jpg"))
+            val number = imageClassifier?.classify(bitmap)
+            Toast.makeText(this, "値は" + number, Toast.LENGTH_LONG).show()
+        }
+
+        findViewById<Button>(R.id.classify_btn2).setOnClickListener{
+            val bitmap = BitmapFactory.decodeStream(assets.open("sampleimages/test4.jpg"))
+            val number = imageClassifier?.classify(bitmap)
+            Toast.makeText(this, "値は" + number, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        imageClassifier = ImageClassifier(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        imageClassifier?.close()
     }
 }
--- app/src/main/java/com/lyricaloriginal/mnistandroid/recoginitions/ImageClassifier.kt
+++ app/src/main/java/com/lyricaloriginal/mnistandroid/recoginitions/ImageClassifier.kt
@@ -7,16 +7,51 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface
 /**
  * Created by LyricalMaestro0 on 2018/01/14.
  */
-class ImageClassifier{
+class ImageClassifier {
 
-    private var inferenceInterface : TensorFlowInferenceInterface
+    private var intValues: IntArray
+    private var inputFloats: FloatArray
+    private var outputFloats: FloatArray
 
-    constructor(context : Context) {
-        inferenceInterface = TensorFlowInferenceInterface(context.assets, "modelname")
+    private var inferenceInterface: TensorFlowInferenceInterface
+
+    constructor(context: Context) {
+        inferenceInterface = TensorFlowInferenceInterface(context.assets, "modeldata.pb")
+        intValues = IntArray(28 * 28, { 0 })
+        inputFloats = FloatArray(28 * 28, { 0.0f })
+        outputFloats = FloatArray(10, { 0.0f })
     }
 
-    fun classify(inputImage : Bitmap) : Int{
-        return -1
+    fun classify(inputImage: Bitmap): Int {
+        //  入力画像の前処理
+        inputImage.getPixels(intValues, 0, inputImage.width, 0, 0, inputImage.width, inputImage.height)
+        for (index in intValues.indices) {
+            val r = (intValues[index] shr 16) and 0xFF
+            val g = (intValues[index] shr 8) and 0xFF
+            val b = (intValues[index]) and 0xFF
+            inputFloats[index] = (r + g + b).toFloat() / 3.0f
+        }
+
+        //  予測
+        inferenceInterface.feed("dense_1_input", inputFloats, 1, 28 * 28)
+        inferenceInterface.run(arrayOf("output_node0"))
+        inferenceInterface.fetch("output_node0", outputFloats)
+
+        //  確信度が一番赤いindexを「予測する数字」とする。
+        var minConfidence = Float.MIN_VALUE
+        var minValue = -1
+        for (index in outputFloats.indices){
+            val confidence = outputFloats[index]
+            if(minConfidence < confidence){
+                minConfidence = confidence
+                minValue = index
+            }
+        }
+       return minValue
+    }
+
+    fun close(){
+        inferenceInterface.close()
     }
 
 }
