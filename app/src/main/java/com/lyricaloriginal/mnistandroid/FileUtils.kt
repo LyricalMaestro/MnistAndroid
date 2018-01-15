a2ef634e6719418fa93843c7de680166318868bd
LyricalMaestro <shinichitanimoto0120@gmail.com>
2018-01-16 00:15:53 +0900

    MNISTサンプルアプリの作成。

---
+++ app/src/main/java/com/lyricaloriginal/mnistandroid/FileUtils.kt
@@ -0,0 +1,37 @@
+package com.lyricaloriginal.mnistandroid
+
+import android.content.res.AssetManager
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+
+/**
+ * Created by shinichitanimoto on 2018/01/15.
+ */
+fun copyDirectory(assetManager: AssetManager, fromFilter : String, to: File) {
+    if (!to.exists()) {
+        to.mkdirs()
+    }
+
+    var fromFilenames = assetManager.list(fromFilter)
+    if (fromFilenames.size > 0) {
+        for (fromFilename in fromFilenames) {
+            val toFile = File(to, fromFilename)
+            assetManager.open(fromFilter + "/" + fromFilename).use {
+                copyFile(it, toFile)
+            }
+        }
+    }
+}
+
+fun copyFile(from : InputStream, to :File){
+    var len : Int = 0
+    var buf : ByteArray = kotlin.ByteArray(256)
+    FileOutputStream(to).use{
+        len = from.read(buf, 0, 256)
+        while(len != -1){
+            it.write(buf, 0, len)
+            len = from.read(buf, 0, 256)
+        }
+    }
+}
--- app/src/main/java/com/lyricaloriginal/mnistandroid/MainActivity.kt
+++ app/src/main/java/com/lyricaloriginal/mnistandroid/MainActivity.kt
@@ -1,31 +1,43 @@
 package com.lyricaloriginal.mnistandroid
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
 import com.lyricaloriginal.mnistandroid.recoginitions.ImageClassifier
+import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener{
 
     var imageClassifier : ImageClassifier? = null
+    var imageFiles : List<File>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.classify_btn1).setOnClickListener{
-            val bitmap = BitmapFactory.decodeStream(assets.open("sampleimages/test0.jpg"))
-            val number = imageClassifier?.classify(bitmap)
-            Toast.makeText(this, "値は" + number, Toast.LENGTH_LONG).show()
+        if(savedInstanceState == null){
+            copyImage()
         }
+        imageFiles = getExternalFilesDir(Environment.DIRECTORY_DCIM).listFiles().asList()
 
-        findViewById<Button>(R.id.classify_btn2).setOnClickListener{
-            val bitmap = BitmapFactory.decodeStream(assets.open("sampleimages/test4.jpg"))
-            val number = imageClassifier?.classify(bitmap)
-            Toast.makeText(this, "値は" + number, Toast.LENGTH_LONG).show()
-        }
+        var gridView : GridView = findViewById(R.id.target_image_grid_view)
+        gridView.adapter = MnistAdapter(this, imageFiles)
+        gridView.onItemClickListener = this
+    }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val imageFile = imageFiles!![p2]
+        val inputImage = BitmapFactory.decodeFile(imageFile.absolutePath)
+        val category = imageClassifier!!.classify(inputImage)
+        updateUi(inputImage, category)
     }
 
     override fun onResume() {
@@ -36,5 +48,19 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         imageClassifier?.close()
+    }
+
+    private fun copyImage(){
+        val outputDir = getExternalFilesDir(Environment.DIRECTORY_DCIM)
+
+        copyDirectory(assets, "sampleimages", outputDir)
+    }
+
+    private fun updateUi(selectedImage : Bitmap, category : Int){
+        val imageView : ImageView = findViewById(R.id.selected_image_view)
+        imageView.setImageBitmap(selectedImage)
+
+        val textView : TextView = findViewById(R.id.result_text_view)
+        textView.setText("answer is " + category)
     }
 }
